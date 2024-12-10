6b8f912a2bc01a58881450f3981ef4edca8d3ef7
Luis Armando <mariana@MacBook-Air-de-Luis-Armando.local>
2024-12-10 15:40:14 -0800

    more comments

--- tiny_test.py
+++ tiny_test.py
@@ -22,4 +22,12 @@ for naturalRestrictedSentence in natural_restricted_sentences_auth_context:
     else:
         print("❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌")
 
-# don't use the non_statements when typing conditions
+# Area of improvement (sort of): don't use the non_statements when typing conditions
+# Area of improvement: variable names are limited to 26ish, they need to be numbered so they can be infinite
+# (although statements are not infinite unless 
+# they are the reflection of an infinite reality, 
+# but finite observers who get to be finite in the end, whitin that infinite "reality" [which only an observer can
+# transform it into real as it categorizes it as such],
+# don't need an infinite implementation, 
+# but still it could be cool to have it, 
+# eventhough a finite system such as a 2024 laptop is finite in computing power)
